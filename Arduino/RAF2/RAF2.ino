5d83a6ad3f4f89bb298ba62b9b13f9464e62a9b0
osbock <kevin.osborn@gmail.com>
2017-08-09 15:27:53 -0400

    add FastLED copy, update RAF2 example

--- Arduino/RAF2/RAF2.ino
+++ Arduino/RAF2/RAF2.ino
@@ -1,4 +1,4 @@
-#include <Adafruit_NeoPixel.h>
+#include <FastLED.h>
 
 #include <Servo.h>
 
@@ -13,17 +13,19 @@
 #define LIGHT A2
 #define TEMP A3
 #define NEOPIXELPIN 8
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(3, NEOPIXELPIN, NEO_GRB + NEO_KHZ800);
-
-
+#define NUM_LEDS 8
+CRGB leds[NUM_LEDS];
+#define DATA_PIN NEOPIXELPIN
+#define BRIGHTNESS 20
 Servo xServo,yServo;
 void setup() {
+  FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
+  FastLED.setBrightness(BRIGHTNESS);
+
   Serial.begin(115200);
   Serial.println("Ready Aim Fire!");
   
-  strip.begin();
-  strip.setBrightness(20);
-  strip.show(); // Initialize all pixels to 'off'
+
   xServo.attach(SERV1);
   yServo.attach(SERV2);
   yServo.write(90);
@@ -44,42 +46,27 @@ void setup() {
 
  
 }
+void chase()
+{
+     // Move a single white led 
+   for(int whiteLed = 0; whiteLed < NUM_LEDS; whiteLed = whiteLed + 1) {
+      // Turn our current led on to white, then show the leds
+      leds[whiteLed] = CRGB::White;
 
+      // Show the leds (only one of which is set to white, from above)
+      FastLED.show();
+
+      // Wait a little bit
+      delay(100);
+
+      // Turn our current led back to black for the next loop around
+      leds[whiteLed] = CRGB::Black;
+      FastLED.show();
+   }
+}
 int lastx;
 int lasty;
-void blank() // black out the neopixel strip
-{
-  for (int i = 0; i < strip.numPixels();i++)
-    strip.setPixelColor(i,0);
 
-   strip.show();
-}
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  if(WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-}
-// Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
-}
 void loop() {
 
   
@@ -89,8 +76,7 @@ void loop() {
     digitalWrite(SOLENOID,HIGH);
     delay(250);
     digitalWrite(SOLENOID,LOW);
-    rainbowCycle(10);
-    blank();
+    chase();
     digitalWrite(PUMP,HIGH);
     delay(4000);
     digitalWrite(PUMP, LOW);
@@ -98,13 +84,13 @@ void loop() {
   }
   int light = analogRead(LIGHT);
   int temp = analogRead(TEMP);
-  Serial.print("LIGHT: ");Serial.print(light);Serial.print(" TEMP: ");Serial.println(temp);
+  //Serial.print("LIGHT: ");Serial.print(light);Serial.print(" TEMP: ");Serial.println(temp);
   int x = analogRead(JOYX);
   int y = analogRead(JOYY);
 
   if(abs(lastx-x)>2)
   {
-    //Serial.print("X: ");Serial.println(x);
+    Serial.print("X: ");Serial.println(x);
     xServo.write(x/4);
     lastx =x;
   }
@@ -112,12 +98,12 @@ void loop() {
   {
 
     // Y is also around 450-500 but y axis is at 90 essentially at rest,  so map forward and back;
-    //Serial.print("Y: ");Serial.println(y);
+    Serial.print("Y: ");Serial.println(y);
     if (y < 523)
     {
-      yServo.write(map(y,0,523,1,80));
+      yServo.write(map(y,0,518,1,100));
     }else
-      yServo.write(map(y,524,1023,81,110));
+      yServo.write(map(y,519,1023,101,130));
     lasty = y;
   }
 
