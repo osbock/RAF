4ec17ab367e112653619551e8defe06732422db3
Kevin Osborn <Kevin.Osborn@capitalone.com>
2018-05-22 16:14:04 -0400

    update raf2 for new servos

--- Arduino/RAF2/RAF2.ino
+++ Arduino/RAF2/RAF2.ino
@@ -13,11 +13,12 @@
 #define LIGHT A2
 #define TEMP A3
 #define NEOPIXELPIN 8
-#define NUM_LEDS 9
+#define NUM_LEDS 1
 CRGB leds[NUM_LEDS];
 #define DATA_PIN NEOPIXELPIN
 #define BRIGHTNESS 20
-Servo xServo,yServo;
+Servo azimuth;
+Servo elevation;
 void setup() {
   FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
   FastLED.setBrightness(BRIGHTNESS);
@@ -26,9 +27,10 @@ void setup() {
   Serial.println("Ready Aim Fire!");
   
 
-  xServo.attach(SERV1);
-  yServo.attach(SERV2);
-  yServo.write(3);
+  azimuth.attach(SERV1);
+  elevation.attach(SERV2);
+  azimuth.write(80);
+  elevation.write(3);
   pinMode(SOLENOID,OUTPUT);
   digitalWrite(SOLENOID,LOW);
   pinMode(PUMP,OUTPUT);
@@ -73,7 +75,11 @@ int lasty;
 void loop() {
 
   
-  if (digitalRead(BUTTON) == LOW)
+ 
+  int light = analogRead(LIGHT);
+  int temp = analogRead(TEMP);
+  //Serial.print("LIGHT: ");Serial.print(light);Serial.print(" TEMP: ");Serial.println(temp);
+   if (digitalRead(BUTTON) == LOW)
   {
     Serial.println("fire!");
     digitalWrite(SOLENOID,HIGH);
@@ -85,29 +91,34 @@ void loop() {
     digitalWrite(PUMP, LOW);
     delay(100);
   }
-  int light = analogRead(LIGHT);
-  int temp = analogRead(TEMP);
-  //Serial.print("LIGHT: ");Serial.print(light);Serial.print(" TEMP: ");Serial.println(temp);
-  int x = analogRead(JOYX);
-  int y = analogRead(JOYY);
+ int xReading,yReading;
+  xReading = analogRead(JOYX);
+  yReading = analogRead(JOYY);
 
-  if(abs(lastx-x)>2)
+if(abs(lastx-xReading) > 2)
+{
+  if (xReading <490)
   {
-    Serial.print("X: ");Serial.println(x);
-    xServo.write(x/4);
-    lastx =x;
+    azimuth.write(map(xReading,490,0,90,2));
+  }else
+  if (xReading > 489)
+  {
+    azimuth.write(map(xReading,489,1023,91,178));
   }
-  if(abs(lasty-y)>2)
-  {
+  lastx = xReading;
 
-    // Y is also around 450-500 but y axis is at 90 essentially at rest,  so map forward and back;
-    Serial.print("Y: ");Serial.println(y);
-    if (y < 523)
-    {
-     yServo.write(map(y,523,0,31,70));
-    }else
-      yServo.write(map(y,524,1023,30,3));
-    lasty = y;
+}
+if (abs(lasty-yReading) > 2)
+{
+  if (yReading <515)
+  {
+    elevation.write(map(yReading,515,0,31,70));
+  }else
+  if (yReading > 514)
+  {
+    elevation.write(map(yReading,524,1023,30,3));
   }
+  lasty = yReading;
+}
 
 }
